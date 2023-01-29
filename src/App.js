154de6426a00b61a5bcc5af74634922a6582f067
charanvinay <charanvinaynarni@gmail.com>
2023-01-29 16:20:12 +0530

    slices deleted

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './component/home';
 import Navbar from './component/navbar';
-import {Provider} from "react-redux"
-import {store} from "./redux/store"
 
 function App() {
   return (
-    <Provider store={store}>
+    <>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
-    </Provider>
+    </>
   );
 }
 
